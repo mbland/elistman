7e222ef88bb3bc83c14a1b5ca478472195914ba9
Mike Bland <mbland@acm.org>
2023-03-25 15:19:00 -0400

    Reinstate 'go install staticcheck' in check-tools
    This reverts part of commit 76c2b9f83219be4550b8f020d20234836935a3f4.
    
    'go build' won't install modules from 'tools.go', meaning one would have
    to run 'go run path/to/package'. That can be annoying for local
    development, so I reinstated the 'go install' for shellcheck.
    
    Note that the Go extension for VS Code doesn't automatically try to
    fetch the 'shellcheck' package, either, though it's the default linter.

--- tools.go
+++ tools.go
@@ -1,6 +1,5 @@
 //go:build tools
 
-//
 // See:
 // - https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // - https://play-with-go.dev/tools-as-dependencies_go119_en/
