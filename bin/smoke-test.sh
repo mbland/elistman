f40b9f267c1dcfde96282ded020f7a90bd8eb99d
Mike Bland <mbland@acm.org>
2023-03-29 17:11:23 -0400

    Use single quotes where possible in smoke-test.sh
    Their use was previously inconsistent. It's nice to see the single
    quotes and know there's no substitution happening in any given string.
    The converse assumption being that double quoted strings do perform
    substitution.
    
    The one exception is the final printf_fail call in
    expect_status_from_endpoint. Replacing the double quotes around the
    format string threw off the syntax highlighting for the rest of the
    file.

--- bin/smoke-test.sh
+++ bin/smoke-test.sh
@@ -2,14 +2,14 @@
 #
 # Checks that the service is running responding to requests.
 
-BASE_URL="https://api.mike-bland.com/email"
+BASE_URL='https://api.mike-bland.com/email'
 
-if [[ "$1" == "--local" ]]; then
+if [[ "$1" == '--local' ]]; then
   if [[ -z "$(ps -ef | grep -- '[s]am local start-api --port 8080')" ]]; then
-    printf '%s\n' "Please run 'make run-local' in a separate shell first." >&2
+    printf '%s\n' 'Please run 'make run-local' in a separate shell first.' >&2
     exit 1
   fi
-  BASE_URL="http://127.0.0.1:8080"
+  BASE_URL='http://127.0.0.1:8080'
   LOCAL=1
 fi
 
@@ -21,9 +21,9 @@ printf_with_highlight() {
   shift 2
 
   if [[ -t 1 ]]; then
-    printf "%b%s" "${style_code}" "${prefix}"
+    printf '%b%s' "${style_code}" "${prefix}"
     printf "$@"
-    printf "%b" '\033[0m'
+    printf '%b' '\033[0m'
   else
     printf "*** ${prefix}: "
     printf "$@"
@@ -31,15 +31,15 @@ printf_with_highlight() {
 }
 
 printf_info() {
-  printf_with_highlight '\033[1;36m' "INFO: " "$@"
+  printf_with_highlight '\033[1;36m' 'INFO: ' "$@"
 }
 
 printf_pass() {
-  printf_with_highlight '\033[1;32m' "PASSED: " "$@"
+  printf_with_highlight '\033[1;32m' 'PASSED: ' "$@"
 }
 
 printf_fail() {
-  printf_with_highlight '\033[1;31m' "FAILED: " "$@"
+  printf_with_highlight '\033[1;31m' 'FAILED: ' "$@"
 }
 
 expect_status_from_endpoint() {
@@ -49,11 +49,11 @@ expect_status_from_endpoint() {
   local endpoint="${BASE_URL}/${4}"
   local content_type="$5"
   local num_shift_args="$(($# < 5 ? $# : 5))"
-  local curl_data_flag="-d"  # for application/x-www-form-urlencoded
+  local curl_data_flag='-d'  # for application/x-www-form-urlencoded
   local postdata=()
 
-  if [[ "$content_type" == "multipart/form-data" ]]; then
-    curl_data_flag="-F"
+  if [[ "$content_type" == 'multipart/form-data' ]]; then
+    curl_data_flag='-F'
   fi
   shift "$num_shift_args"
 
@@ -61,22 +61,22 @@ expect_status_from_endpoint() {
     postdata+=("$curl_data_flag" "$arg")
   done
 
-  printf_info "TEST: %s\nExpect %s from: %s %s\n" \
+  printf_info 'TEST: %s\nExpect %s from: %s %s\n' \
     "$description" "$status" "$method" "$endpoint"
 
-  local curl_cmd=("curl" "-isS" "-X" "$method" "${postdata[@]}" "$endpoint")
+  local curl_cmd=('curl' '-isS' '-X' "$method" "${postdata[@]}" "$endpoint")
   local response="$("${curl_cmd[@]}")"
 
-  printf "%s\n\n%s\n" "${curl_cmd[*]}" "${response/%$'\n'}"
-  local response_status=""
+  printf '%s\n\n%s\n' "${curl_cmd[*]}" "${response/%$'\n'}"
+  local response_status=''
 
   if [[ "$response" =~ HTTP/[^\ ]+\ ([1-5][0-9][0-9]) ]]; then
     response_status="${BASH_REMATCH[1]}"
 
     if [[ "$response_status" == "$status" ]]; then
-      printf_pass "%s: %s\n\n" "$description" "$status"
+      printf_pass '%s: %s\n\n' "$description" "$status"
     else
-      printf_fail "%s: Expected %s, actual %s\n\n" \
+      printf_fail '%s: Expected %s, actual %s\n\n' \
         "$description" "$status" "$response_status"
       ((EXIT_CODE+=1))
     fi
@@ -87,7 +87,7 @@ expect_status_from_endpoint() {
   fi
 }
 
-printf_info "SUITE: Success\n"
+printf_info 'SUITE: Success\n'
 expect_status_from_endpoint \
   'successful subscribe using urlencoded params' \
   303 POST \
@@ -101,13 +101,13 @@ expect_status_from_endpoint \
   'multipart/form-data' \
   'email=mbland%40acm.org'
 expect_status_from_endpoint \
-  "successful verify" \
+  'successful verify' \
   303 GET \
   'verify/mbland%40acm.org/00000000-1111-2222-3333-444444444444'
 expect_status_from_endpoint \
-  "unsubscribe" \
+  'unsubscribe' \
   303 GET \
-  "unsubscribe/mbland%40acm.org/00000000-1111-2222-3333-444444444444"
+  'unsubscribe/mbland%40acm.org/00000000-1111-2222-3333-444444444444'
 expect_status_from_endpoint \
   'one-click unsubscribe using urlencoded params' \
   200 POST \
@@ -117,11 +117,11 @@ expect_status_from_endpoint \
 expect_status_from_endpoint \
   'one-click unsubscribe using form-data' \
   200 POST \
-  "unsubscribe/mbland%40acm.org/00000000-1111-2222-3333-444444444444" \
+  'unsubscribe/mbland%40acm.org/00000000-1111-2222-3333-444444444444' \
   'multipart/form-data' \
   'List-Unsubscribe=One-Click'
 
-printf_info "SUITE: Not found (403 locally, 404 in prod)\n"
+printf_info 'SUITE: Not found (403 locally, 404 in prod)\n'
 
 not_found_status=404
 if [[ -n "$LOCAL" ]]; then
@@ -129,12 +129,12 @@ if [[ -n "$LOCAL" ]]; then
 fi
 
 expect_status_from_endpoint \
-  "invalid endpoint not found" \
+  'invalid endpoint not found' \
   "$not_found_status" POST \
   'foobar/mbland%40acm.org'
 
-printf_info "%s\n" \
-  "SUITE: Redirect if missing or invalid email address for /subscribe"
+printf_info '%s\n' \
+  'SUITE: Redirect if missing or invalid email address for /subscribe'
 
 missing_address_status=303
 if [[ -n "$LOCAL" ]]; then
@@ -142,29 +142,29 @@ if [[ -n "$LOCAL" ]]; then
 fi
 
 expect_status_from_endpoint \
-  "missing email address (403 locally, 303 in prod)" \
+  'missing email address (403 locally, 303 in prod)' \
   "$missing_address_status" POST \
   'subscribe'
 expect_status_from_endpoint \
-  "invalid email address" \
+  'invalid email address' \
   303 POST \
   'subscribe'
   'application/x-www-form-urlencoded' \
   'email=foo%20bar'
 
-printf_info "SUITE: All other missing or invalid parameters return 400\n"
+printf_info 'SUITE: All other missing or invalid parameters return 401\n'
 expect_status_from_endpoint \
-  "invalid email address for /verify" \
+  'invalid email address for /verify' \
   400 GET \
   'verify/foobar/00000000-1111-2222-3333-444444444444'
 expect_status_from_endpoint \
-  "invalid UID for /unsubscribe" \
+  'invalid UID for /unsubscribe' \
   400 GET \
   'unsubscribe/mbland%40acm.org/bad-uid'
 
 if [[ "$EXIT_CODE" -eq 0 ]]; then
-  printf_pass "All smoke tests passed!\n"
+  printf_pass 'All smoke tests passed!\n'
 else
-  printf_fail "Some expectations failed; see above output for details.\n"
+  printf_fail 'Some expectations failed; see above output for details.\n'
 fi
 exit "$EXIT_CODE"
