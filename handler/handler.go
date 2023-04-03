6dc31a19d7ae7fd3978f4290cbace8dc49411963
Mike Bland <mbland@acm.org>
2023-04-03 16:43:48 -0400

    Very minor handler.ResponseTemplate tweaks

--- handler/handler.go
+++ handler/handler.go
@@ -73,8 +73,8 @@ func initResponseBodyTemplate(bodyTmpl string) (*template.Template, error) {
 const ResponseTemplate = `<!DOCTYPE html>
 <html lang="en-us">
   <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title>{{.Title}} - {{.SiteTitle}}</title>
   </head>
   <body>
