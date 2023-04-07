45c96d7018e648deddbc9916b0bed63306e287e7
Mike Bland <mbland@acm.org>
2023-04-07 19:12:28 -0400

    Add bin/endpoints.sh to show CD pipe API endpoints
    Copied/slightly modified from:
    https://catalog.workshops.aws/complete-aws-sam/en-US/module-4-cicd/module-4-cicd-gh/60-inspect#inspect-the-devprod-stages

---
+++ bin/endpoints.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# From:
+# https://catalog.workshops.aws/complete-aws-sam/en-US/module-4-cicd/module-4-cicd-gh/60-inspect#inspect-the-devprod-stages
+
+function get_endpoint() {
+  local stack_name="$1"
+
+  aws cloudformation describe-stacks --stack-name "$stack_name" |
+    jq -r '.Stacks[].Outputs[].OutputValue | select(startswith("https://"))'
+}
+
+export DEV_ENDPOINT="$(get_endpoint elistman-dev)"
+export PROD_ENDPOINT="$(get_endpoint elistman-prod)"
+
+echo "Dev endpoint: $DEV_ENDPOINT"
+echo "Prod endpoint: $PROD_ENDPOINT"
+
+curl -s $DEV_ENDPOINT
+curl -s $PROD_ENDPOINT
