63c2ac7a4e64975e4c997110338136cecdc54f69
Mike Bland <mbland@acm.org>
2023-05-16 13:00:37 -0400

    Move errors_test.go from ops/ to types/
    Though I'm going to keep types/errors.go, I'm thinking of moving
    types/subscriber.go back to db/. I don't think moving Subscriber to
    types is providing as much benefit after all, and that it really belongs
    in db.
    
    I think there are other means to provide shared test data objects,
    through either build tags, *_test packages, or a combination of the two.

--- types/errors_test.go
+++ types/errors_test.go
@@ -1,6 +1,6 @@
 //go:build small_tests || all_tests
 
-package ops
+package types
 
 import (
 	"testing"
@@ -8,8 +8,10 @@ import (
 	"gotest.tools/assert"
 )
 
+const errTesting = SentinelError("testing")
+
 func TestSentinelError(t *testing.T) {
 	t.Run("ReturnsItselfFromErrorMethod", func(t *testing.T) {
-		assert.Equal(t, string(ErrExternal), ErrExternal.Error())
+		assert.Equal(t, string(errTesting), errTesting.Error())
 	})
 }
