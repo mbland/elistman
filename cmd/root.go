da8d2bdacb90229e9c1d585c941e9bf46854b127
Mike Bland <mbland@acm.org>
2023-06-01 12:55:18 -0400

    Add long description to root elistman command

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const elistmanDesc = "Mailing list system providing address validation " +
+	"and unsubscribe URIs"
+const elistmanDescLong = elistmanDesc + "\n\n" +
+	`See the https://github.com/mbland/elistman README for details.
+
+To create a table:
+  elistman create-subscribers-table <TABLE_NAME>
+
+To see an example of the message input JSON structure:
+  elistman preview --help
+
+To preview a raw message before sending, where ` + "`generate-email`" + ` is any
+program that creates message input JSON:
+  generate-email | elistman preview
+
+To send an email to the list, given the ARN of the elistman Lambda function:
+  generate-email | elistman send <LAMBDA_ARN>
+`
+
 var rootCmd = &cobra.Command{
 	Use:     "elistman",
 	Version: "v0.1.0",
-	Short: "Mailing list system providing address validation " +
-		"and unsubscribe URIs",
-	Long: ``,
+	Short:   elistmanDesc,
+	Long:    elistmanDescLong,
 }
 
 func Execute() error {
